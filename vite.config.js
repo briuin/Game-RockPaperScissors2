8f9b1521fb275f4b400b932687e1480a494c7284
Edward Lim <Edward.Lim@redcypress.com.tw>
2022-04-16 17:08:47 +0800

    initial project

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import federation from "@originjs/vite-plugin-federation";
+// eslint-disable-next-line no-undef
+const deps = require("./package.json").dependencies;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    federation({
+      name: "gameRockPaperScissors2",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Content": "./src/components/HelloWorld.vue",
+      },
+      shared: {
+        ...deps,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
